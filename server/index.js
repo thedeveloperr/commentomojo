77c683a7de572b929c7f87cae17723e5c5d18ac6
Mohit Gupta <mohitgupta3mail@gmail.com>
2019-09-06 02:15:58 +0530

    Change port number for server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
 const app = require('./app');
-const port = 3000;
+const port = 4000;
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
