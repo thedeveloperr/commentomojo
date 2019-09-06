3bd0d27c96256e805bb248b5ab8e90005c6e9588
Mohit Gupta <mohitgupta3mail@gmail.com>
2019-09-06 18:51:43 +0530

    Add db configd for deployment.
    Used sqlite as its a small assignment.

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,7 +1,18 @@
 // Update with your config settings.
 
 module.exports = {
-
+ production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './prod.sqlite3'
+    },
+    pool: {
+    afterCreate: (conn, cb) => {
+      conn.run('PRAGMA foreign_keys = ON', cb)
+    },
+  },
+ },
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
@@ -43,37 +54,4 @@ module.exports = {
   },
 
   }
-/*
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-*/
 };
